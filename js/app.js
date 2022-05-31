47d21de6e1cca51edfe83053f5fbc32f1da0a513
zeynepalaka <zeynepalakaa@gmail.com>
2022-06-01 00:26:07 +0300

    anatolMedia

---
+++ js/app.js
@@ -0,0 +1,31 @@
+ const navSlide= () => {
+    const burger=document.querySelector('.burger');
+    const nav=document.querySelector('.nav-links');
+    const navLinks=document.querySelectorAll('.nav-links li')
+
+
+    burger.addEventListener('click' , ()  => {
+        nav.classList.toggle('nav-active');
+ 
+    navLinks.forEach((link, index)=>{
+      if(link.style.animation){
+          link.style.animation='';
+      }
+      else{
+          link.style.animation='navLinkFade 0.5s ease forwards ${index/7}s'; 
+          
+      }
+
+    });
+    burger.classList.toggle('toggle');
+}); 
+}
+
+
+navSlide(); 
+
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+
+})
